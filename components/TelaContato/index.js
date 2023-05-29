22b2c8ccea39872f3860fa8e4239b6f950ec287d
israekbaruque <israekbaruque992@gmail.com>
2023-05-29 14:34:46 -0300

    wine

---
+++ components/TelaContato/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native-web";
+
+export default function TelaContato () {
+    return (
+        <View>
+            <Text>TelaContato</Text>
+        </View>
+    )
+}
